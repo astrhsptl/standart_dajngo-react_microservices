5c3e415e8f9f0e8f6bf834f6015f53ef20b499d8
astrhsptl <dctrback@bk.ru>
2022-11-06 17:29:05 +0300

    first commit

---
+++ server/authsystem/models.py
@@ -0,0 +1,66 @@
+import jwt
+from django.db import models
+from django.conf import settings 
+from datetime import datetime, timedelta
+from django.contrib.auth.models import (
+	AbstractBaseUser, BaseUserManager,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=256, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Auth settings
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+
+    @property
+    def token(self):
+        dt = datetime.now() + timedelta(days=1)
+        token = jwt.encode({
+            'username': self.username,
+            'email': self.email,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+        return token
